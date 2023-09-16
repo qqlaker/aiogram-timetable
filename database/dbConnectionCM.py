b3b4aecb1d71431330de811966e5e5a5f59e75e9
qqlaker <yayaya.tytytyty@mail.ru>
2023-09-16 20:22:37 +0300

    rm DATABASE_FILE (path) env value

--- database/dbConnectionCM.py
+++ database/dbConnectionCM.py
@@ -1,15 +1,11 @@
 import sqlite3
 import os
 
-from dotenv.main import load_dotenv
-
-load_dotenv("config/.env")
-
 
 class DatabaseConnection(object):
     def __init__(
             self,
-            database=os.getenv("DATABASE_FILE")
+            database="database/sqlite.db"
     ):
         self.connection = None
         self.database = database
